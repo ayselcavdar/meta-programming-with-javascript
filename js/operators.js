a2ad2d6f26c418def6acf257c896b512b1a53c67
Aysel Cavdar <a.misrikhanova13@gmail.com>
2023-12-28 20:15:53 +0400

    working with operators

---
+++ js/operators.js
@@ -0,0 +1,20 @@
+/* Arithmetical operators in JS:
++ addition
+-substraction
+* multiplication
+/ division
+*/
+
+/* Comparison operators in JS:
+> greater than
+< less than
+== equal
+*/
+
+console.log(2 + 2);
+console.log(1 + 2 + 5);
+console.log(20 - 18);
+console.log(2 * 3);
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
