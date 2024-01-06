0e51d350a46dcd7d4b159900b85d116f41e49582
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-06 18:53:08 +0400

    loop intro

---
+++ js/loop.js
@@ -0,0 +1,19 @@
+//with console.log method
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+
+//for loop method
+for (var i = 1; i < 7; i++) {
+  console.log(i);
+}
+
+//while loop method
+var i = 1;
+while (i < 7) {
+  console.log(i);
+  i = i + 1;
+}
