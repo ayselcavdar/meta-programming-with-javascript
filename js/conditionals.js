0a4a0b14e58999550ee87ecbdd9cd57189a013a8
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-06 14:20:20 +0400

    if, else if, else statements

--- js/conditionals.js
+++ js/conditionals.js
@@ -6,3 +6,16 @@ if (result > 40) {
 } else {
   console.log("You did not pass the test");
 }
+
+// if, else if ,else
+var place = "first";
+
+if (place == "first") {
+  console.log("Gold");
+} else if (place == "second") {
+  console.log("Silver");
+} else if (place == "third") {
+  console.log("Bronze");
+} else {
+  console.log("No Medal");
+}
