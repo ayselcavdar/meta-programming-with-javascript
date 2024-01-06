d3ccecb1ba7c730aa31344a1c1adc3bfe716f4f0
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-06 18:17:46 +0400

    examples with if else and switch

--- js/conditionals.js
+++ js/conditionals.js
@@ -36,3 +36,44 @@ switch (place) {
   default:
     console.log("No Medal");
 }
+
+//example with if else
+var age = 18;
+if (age > 65) {
+  console.log("You get your income from your pension");
+} else if (age < 65 && age >= 18) {
+  console.log("Each month you get a salary");
+} else if (age < 18) {
+  console.log("You get an allowance");
+} else {
+  console.log("The value of the age variable is not numerical");
+}
+
+//example with switch
+var day = "Sunday";
+switch (day) {
+  case "Monday":
+    console.log("Read a book");
+    break;
+  case "Tuesday":
+    console.log("Watch a movie");
+    break;
+  case "Wednesday":
+    console.log("Read a book");
+    break;
+  case "Thursday":
+    console.log("Play basketball");
+    break;
+  case "Friday":
+    console.log("Socialize");
+    break;
+  case "Saturday":
+    console.log("Chill");
+    break;
+  case "Sunday":
+    console.log("Have barbecue");
+    break;
+  default:
+    //this block will run if no condition matches
+    console.log("There is no such day");
+}
