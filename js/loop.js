c965d846fff03bfe5b710d4eef1ba827281c5016
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-08 14:06:05 +0400

    working with loops

--- js/loop.js
+++ js/loop.js
@@ -32,3 +32,66 @@ for (var i = 2023; i < 2026; i++) {
     console.log("---------", j);
   }
 }
+
+//examples
+//single loop
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+  console.log(firstNum);
+}
+
+for (var secondNum = 0; secondNum < 10; secondNum++) {
+  console.log(secondNum);
+}
+
+//nested loop - one inside another
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+  for (var secondNum = 0; secondNum < 10; secondNum++) {
+    console.log(firstNum + ", " + secondNum);
+  }
+}
+
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+  for (var secondNum = 0; secondNum < 10; secondNum++) {
+    console.log(
+      firstNum + " times " + secondNum + " equals " + firstNum * secondNum
+    );
+  }
+}
+
+for (var i = 100; i > 10; i = i - 10) {
+  for (var j = 10; j > 4; j = j - 5) {
+    console.log(i + " divided by " + j + " equals " + i / j);
+  }
+}
+
+//Exercise 1
+for (var i = 1; i <= 10; i++) {
+  if (i == 1) {
+    console.log("Gold medal");
+  } else if (i == 2) {
+    console.log("Silver medal");
+  } else if (i == 3) {
+    console.log("Bronze medal");
+  } else {
+    //this block will run if no condition matches
+    console.log(i);
+  }
+}
+
+//Exercise 2
+for (var i = 1; i <= 10; i++) {
+  switch (i) {
+    case 1:
+      console.log("Gold medal");
+      break;
+    case 2:
+      console.log("Silver medal");
+      break;
+    case 3:
+      console.log("Bronze medal");
+      break;
+    default:
+      //this block will run if no condition matches
+      console.log(i);
+  }
+}
