9ce430248feb41d66936a963ef65ba2833b05cb3
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-08 17:16:40 +0400

    Building and calling functions

--- js/function.js
+++ js/function.js
@@ -19,3 +19,24 @@ function addTwoNums(a, b) {
 }
 addTwoNums(10, 20);
 addTwoNums(5, 9);
+
+//Building and calling functions
+//function that takes an array as input and display all items of this array
+function listArrayItems(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(i, arr[i]);
+  }
+}
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+listArrayItems(colors);
+
+//with conditions
+function listArrayItems(arr) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == "red") {
+      console.log(i * 100, "tomato");
+    } else {
+      console.log(i * 100, arr[i]);
+    }
+  }
+}
