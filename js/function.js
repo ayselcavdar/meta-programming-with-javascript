cf1ab81492cb6a0d5f7782105809ad133bd52a0e
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-08 14:06:34 +0400

    function intro

---
+++ js/function.js
@@ -0,0 +1,21 @@
+function myFunc() {
+  console.log("hello");
+}
+myFunc();
+
+//function without parameters
+function addTwoNums() {
+  var a = 10;
+  var b = 20;
+  var c = a + b;
+  console.log(c);
+}
+addTwoNums();
+
+//function with parameters
+function addTwoNums(a, b) {
+  var c = a + b;
+  console.log(c);
+}
+addTwoNums(10, 20);
+addTwoNums(5, 9);
