6be22e7eefa40027ae0bdd1306a3c53edba75ca8
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-09 21:51:34 +0400

    Math object

---
+++ js/mathRandom.js
@@ -0,0 +1,18 @@
+//Math.random()
+
+//Generate a decimal number between 0 and 0.99
+Math.random();
+
+//Save it to a variable:
+var decimal = Math.random();
+console.log(decimal);
+console.log(decimal * 10);
+
+//Math.ceil()
+var rounded = Math.ceil(0.0001);
+console.log(rounded);
+
+//example
+var decimal = Math.random() * 10;
+var rounded = Math.ceil(decimal);
+console.log(rounded);
