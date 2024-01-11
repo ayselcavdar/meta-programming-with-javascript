f4786bde584dfe9003457aa77360420b6233c8f0
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-11 17:05:11 +0400

    example of adding properties and methods to an object

--- js/objects.js
+++ js/objects.js
@@ -77,3 +77,18 @@ function arrayBuilder(one, two, three) {
 arrayBuilder();
 var simpleArr = arrayBuilder("apple", "pear", "plum");
 console.log(simpleArr);
+
+//example of adding properties and methods to an object
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function () {
+  console.log("The engine is running");
+};
+car.lightsOn = function () {
+  console.log("The lights are on.");
+};
+console.log(car);
+car.turnTheKey();
+car.lightsOn();
