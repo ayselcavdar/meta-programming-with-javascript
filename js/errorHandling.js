d50f290d3b386410ffc03b2ab5a8f423e1473df8
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-12 21:45:39 +0400

    Exercise: Error prevention

--- js/errorHandling.js
+++ js/errorHandling.js
@@ -22,3 +22,35 @@ try {
   console.log("There was a Reference Error");
 }
 console.log("My program does not stop");
+
+//Exercise: Error prevention
+function addTwoNums(a, b) {
+  console.log(a + b);
+}
+addTwoNums(5, "5");
+
+//Update the addTwoNums function with a try...catch block
+function addTwoNums(a, b) {
+  try {
+    console.log(a + b);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// If the passed-in arguments are not numbers, throw an error
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != "number") {
+      throw new ReferenceError("the first argument is not a number");
+    } else if (typeof b != "number") {
+      throw new ReferenceError("the second argument is not a number");
+    } else {
+      console.log(a + b);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+addTwoNums(5, "5");
+console.log("It still works");
