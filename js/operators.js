c29c3fed8d53b6b7b151fffed7988b2621bd4f24
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-12 19:22:54 +0400

    typeof operator

--- js/operators.js
+++ js/operators.js
@@ -18,3 +18,18 @@ console.log(2 * 3);
 console.log(3 > 2);
 console.log(2 > 3);
 console.log(10 == 10);
+
+//var test = typeof "what is it?";
+//var test = typeof 10;
+//var test = typeof 3.14;
+// var test = typeof true;
+// var test = typeof (1 < 2);
+// var test = typeof [1, 2, 3];
+// var test = typeof [{ firsArray: 1 }];
+var test = typeof [
+  function abc() {
+    console.log("abc");
+  },
+];
+
+console.log(test);
