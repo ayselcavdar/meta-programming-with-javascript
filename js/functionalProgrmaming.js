c1421d050a35b159ac97267fd89d4e9bf41433a0
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-15 20:31:58 +0400

    functional programming and oop

--- js/functionalProgrmaming.js
+++ js/functionalProgrmaming.js
@@ -66,3 +66,46 @@ score();
  * They are accessible only to functions located
  *  within the local scope.
  */
+
+//The functional programming paradigm
+//In functional programming, we use a lot of functions and variables.
+function getTotal(a, b) {
+  return a + b;
+}
+var num1 = 2;
+var num2 = 3;
+
+var total = getTotal(num1, num2);
+console.log(total);
+
+/**
+ * When writing FP code, we keep data and functionality
+ * separate and pass data into functions only when we
+ *  want something computed.
+ */
+function getDistance(mph, h) {
+  return mph * h;
+}
+var mph = 60;
+var h = 2;
+var distance = getDistance(mph, h);
+console.log(distance);
+
+//object-oriented programming (OOP)
+var virtualPet = {
+  sleepy: true,
+  nap: function () {
+    this.sleepy = false;
+  },
+};
+console.log(virtualPet.sleepy); // true
+virtualPet.nap();
+console.log(virtualPet.sleepy); // false
+
+/**
+ * To summarize this point, we can say that the
+ * Functional Programming paradigm works by keeping
+ * the data and functionality separate. It's counterpart,
+ *  OOP, works by keeping the data and functionality
+ * grouped in meaningful objects.
+ */
