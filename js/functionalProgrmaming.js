8a8fd77699b0f2e794eee2da58ddd83ce3bf7bf4
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-16 21:32:56 +0400

    First-class functions

--- js/functionalProgrmaming.js
+++ js/functionalProgrmaming.js
@@ -109,3 +109,34 @@ console.log(virtualPet.sleepy); // false
  *  OOP, works by keeping the data and functionality
  * grouped in meaningful objects.
  */
+
+//There are many more concepts and ideas in functional programming.
+//First-class functions
+function addTwoNums(a, b) {
+  console.log(a + b);
+}
+
+function randomNum() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+function specificNum() {
+  return 42;
+}
+
+var useRandom = true;
+
+var getNumber;
+
+if (useRandom) {
+  getNumber = randomNum;
+} else {
+  getNumber = specificNum;
+}
+
+addTwoNums(getNumber(), getNumber());
+
+/**
+ * This works because functions in JavaScript are truly first-class
+ * citizens, which can be assigned to variable names and passed around
+ * just like I would pass around a string, a number, an object, etc
+ */
