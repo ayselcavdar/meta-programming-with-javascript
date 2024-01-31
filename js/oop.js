979d8e09026ddd58b8425af93b50471d69aa38e8
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-01-31 17:13:48 +0400

    Default Parameters OOP

--- js/oop.js
+++ js/oop.js
@@ -24,9 +24,29 @@ purchase1.totalPrice();
 var purchase2 = {
   shoes: 50,
   stateTax: 1.2,
-  totalPrice: function () {
+  totalPrice1: function () {
     var calculation = this.shoes * this.stateTax;
     console.log("Total price:", calculation);
   },
 };
-purchase2.totalPrice();
+purchase2.totalPrice1();
+
+//Default Parameters
+class WithDefaultParams {
+  constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    this.string1 = string1;
+    this.bool1 = bool1;
+  }
+  calculate() {
+    if (this.bool1) {
+      console.log(this.string1, this.num1 + this.num2 + this.num3);
+      return;
+    }
+    return "The value of bool1 is incorrect";
+  }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6// 'The value of bool1 is incorrect'
